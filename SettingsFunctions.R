45142a00e4246e44da5a984e4c013deb6fa53064
jreps <jreps@its.jnj.com>
2023-02-02 16:00:46 -0500

    adding renv stuff
    - adding renv stuff

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -8,7 +8,7 @@ createPatientLevelPredictionModuleSpecifications <- function(
   
   specifications <- list(
     module = "PatientLevelPredictionModule",
-    version = "0.0.1",
+    version = "0.0.2",
     remoteRepo = "github.com",
     remoteUsername = "ohdsi",
     settings = modelDesignList
