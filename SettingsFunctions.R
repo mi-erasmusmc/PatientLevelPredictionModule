67101d3e20331169d03df1e7423bfe926490a40d
Anthony Sena <asena5@its.jnj.com>
2023-08-13 14:02:23 -0400

    Bump develop version

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -18,7 +18,7 @@
 createPatientLevelPredictionModuleSpecifications <- function(modelDesignList) {
   specifications <- list(
     module = "PatientLevelPredictionModule",
-    version = "0.1.0",
+    version = "0.2.0-1",
     remoteRepo = "github.com",
     remoteUsername = "ohdsi",
     settings = modelDesignList
